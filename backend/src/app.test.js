dcb492a48ee48948098c14ca1d0a32047297e9d9
fortechnicaltests <forgithub019@gmail.com>
2025-12-08 22:48:30 +0000

    add validation tests for task creation

--- backend/src/app.test.js
+++ backend/src/app.test.js
@@ -1,6 +1,6 @@
 import request from 'supertest'
 import app from './app.js'
-import { describe, it, expect} from 'vitest';
+import { describe, it, expect } from 'vitest'
 
 console.log('DATABASE_URL:', process.env.DATABASE_URL, process.cwd())
 
@@ -26,6 +26,64 @@ describe('POST /tasks', () => {
     expect(response.body.task.status).toBe(validTask.status)
     expect(new Date(response.body.task.dueDate).toISOString()).toBe(
       validTask.dueDate
+    )
+  })
+})
+describe('POST /tasks - Validation tests', () => {
+  it('should fail when required fields are missing', async () => {
+    const res = await request(app)
+      .post('/tasks')
+      .send({})
+      .expect(400)
+
+    expect(res.body.message).toBe('Validation failed')
+    expect(res.body.errors).toEqual(
+      expect.arrayContaining([
+        expect.objectContaining({
+          path: ['title'],
+          message: expect.any(String),
+        }),
+        expect.objectContaining({
+          path: ['status'],
+          message: expect.any(String),
+        }),
+        expect.objectContaining({
+          path: ['dueDate'],
+          message: expect.any(String),
+        }),
+      ])
+    )
+  })
+
+  it('should fail when fields have wrong types', async () => {
+    const invalidPayload = {
+      title: 123, 
+      status: true, 
+      dueDate: 20251231, 
+      description: 456,
+    }
+
+    const res = await request(app)
+      .post('/tasks')
+      .send(invalidPayload)
+      .expect(400)
+
+    expect(res.body.message).toBe('Validation failed')
+    expect(res.body.errors).toEqual(
+      expect.arrayContaining([
+        expect.objectContaining({
+          path: ['title'],
+          message: expect.any(String),
+        }),
+        expect.objectContaining({
+          path: ['status'],
+          message: expect.any(String),
+        }),
+        expect.objectContaining({
+          path: ['dueDate'],
+          message: expect.any(String),
+        }),
+      ])
     )
   })
 })
